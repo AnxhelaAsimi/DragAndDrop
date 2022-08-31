097efc758ba24bd8278b89b5de66a5ed08f4a42f
AnxhelaAsimi <87004166+AnxhelaAsimi@users.noreply.github.com>
2022-08-31 11:27:17 +0200

    Fetching User Input

--- src/app.js
+++ src/app.js
@@ -1,11 +1,55 @@
+//autobind decorator
+function autobind(_, _2, descriptor) {
+    var originalMethod = descriptor.value;
+    var adjDescriptor = {
+        configurable: true,
+        get: function () {
+            var boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    };
+    return adjDescriptor;
+}
+//Project Input Class
 var ProjectInput = /** @class */ (function () {
     function ProjectInput() {
         this.templateElement = document.getElementById("project-input");
         this.hostElement = document.getElementById("app");
         var importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild;
+        this.element.id = "user-input";
+        this.titleInputElement = this.element.querySelector("#title");
+        this.descriptionInputELement = this.element.querySelector("#description");
+        this.peopleInputElement = this.element.querySelector("#people");
+        this.configure();
         this.attach();
     }
+    ProjectInput.prototype.gatherUserInput = function () {
+        var enteredTitle = this.titleInputElement.value;
+        var desc = this.descriptionInputELement.value;
+        var people = this.peopleInputElement.value;
+        if (enteredTitle.trim().length === 0 ||
+            desc.trim().length === 0 ||
+            people.trim().length === 0) {
+            alert("Invalid input, please try again!");
+            return;
+        }
+        else {
+            return [enteredTitle, desc, +people];
+        }
+    };
+    // @autobind
+    ProjectInput.prototype.submitHandler = function (event) {
+        event.preventDefault();
+        var userInput = this.gatherUserInput();
+        if (Array.isArray(userInput)) {
+            var title = userInput[0], desc = userInput[1], people = userInput[2];
+            console.log(title, desc, people);
+        }
+    };
+    ProjectInput.prototype.configure = function () {
+        this.element.addEventListener("submit", this.submitHandler.bind(this));
+    };
     ProjectInput.prototype.attach = function () {
         this.hostElement.insertAdjacentElement("afterbegin", this.element);
     };
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,25 @@
+//autobind decorator
+function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
+
+//Project Input Class
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputELement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
+
   constructor() {
     this.templateElement = document.getElementById(
       "project-input"
@@ -13,7 +31,58 @@ class ProjectInput {
       true
     );
     this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = "user-input";
+
+    this.titleInputElement = this.element.querySelector(
+      "#title"
+    ) as HTMLInputElement;
+    this.descriptionInputELement = this.element.querySelector(
+      "#description"
+    ) as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector(
+      "#people"
+    ) as HTMLInputElement;
+
+    this.configure();
     this.attach();
+  }
+
+  private gatherUserInput(): [string, string, number] | void {
+    const enteredTitle = this.titleInputElement.value;
+    const desc = this.descriptionInputELement.value;
+    const people = this.peopleInputElement.value;
+
+    if (
+      enteredTitle.trim().length === 0 ||
+      desc.trim().length === 0 ||
+      people.trim().length === 0
+    ) {
+      alert("Invalid input, please try again!");
+      return;
+    }else{
+      return [enteredTitle, desc, +people];
+    }
+    this.clearInputs();
+  }
+
+  private clearInputs(){
+    this.titleInputElement.value = '';
+    this.descriptionInputELement.value = '';
+    this.peopleInputElement.value = '';
+  }
+
+  // @autobind
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    const userInput = this.gatherUserInput();
+    if(Array.isArray(userInput)){
+      const [title,desc, people]=userInput;
+      console.log(title,desc,people);
+    }
+  }
+
+  private configure() {
+    this.element.addEventListener("submit", this.submitHandler.bind(this));
   }
 
   private attach() {
@@ -21,4 +90,4 @@ class ProjectInput {
   }
 }
 
-const projInput= new ProjectInput();
+const projInput = new ProjectInput();
